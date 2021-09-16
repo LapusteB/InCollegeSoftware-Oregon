d42c05a2d8d2e36c1f67bad992fc9936795e2044
kieu4120 <61288917+kieu4120@users.noreply.github.com>
2021-09-16 13:04:13 -0400

    check password in the right format: minimum of 8 characters, maximum of 12 characters, at least one capital letter, one digit, one non-alpha character

--- validatePass.py
+++ validatePass.py
@@ -1,4 +1,19 @@
-
+#regex library
+import re
 ##use to validate pass in create account
+## email format:
+## minimum of 8 characters, maximum of 12 characters, at least one capital letter, one digit, one non-alpha character
+
+
+#return true if meets requirement else false 
 def validatePass(input):
-    print("validating password")
+    
+    regex = '^(?=.{8,12}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)'
+
+    #return none if not match
+    obj = re.match(regex,input)
+
+    if ( obj != None):
+        return True
+    else: 
+        return False
