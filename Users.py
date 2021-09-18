ce1c37c49dde03826b20b8de5c29591c4e41d485
Brian Lapuste <seblapuste@gmail.com>
2021-09-17 23:37:23 -0400

    Login Finished + Auth

--- Users.py
+++ Users.py
@@ -27,6 +27,7 @@ def register():
     print("Welcome to the InCollege App!")
     print("-----------------------------")
     file = open("usernames.txt", "a") # - file will be created if not present
+    file2 = open("passwords.txt", "a") # - file will be created if not present
     count = 0
 
  #Checks if there are already 5 accounts made this way
@@ -43,19 +44,75 @@ def register():
             home('')
 
     file.write(u + "\n")
+    print("Note password requirements: minimum of 8 characters, maximum of 12 characters, at least one capital letter, one digit, one non-alpha character")
+    p = input("Please enter a unique password: ")
+    with open('passwords.txt') as f:
+        if p in f.read():
+            print("Error, Password already created! Returning home")
+            home('')
+    res = validatePass(p)
+    print(res)
+    while res is False:
+            if res is False:
+                print("Error, Please meet password requirements:")
+                print("---------------------------------------------")
+                print("-minimum of 8 characters")
+                print("-maximum of 12 characters")
+                print("-at least one capital letter")
+                print("-one digit, one non-alpha character")
+                print("---------------------------------------------")
+            p = input("Re-enter Password: ")
+            res = validatePass(p)
+    file2.write(p + "\n")
+    file.close()
+    file2.close()
 
-    #PROMPT PASSWORDS HERE...
+    print("Account Created!")
+    print("Entering main page....")
+    mainPage()
 
-
+  
 def login(user):
     print("--------------------------")
     print("InCollege Login")
     print("--------------------------")
-    u = input("Username: ")
-    p = input("Password: ")
-    mainPage()
-    #TO DO
+    file = open("usernames.txt", "a")
+    file2 = open("passwords.txt", "a")
 
+    u = input("Username: ")
+    with open('usernames.txt') as f:
+        if u in f.read():
+            u = True
+        else:
+            flag = False
+            
+            while flag == False:
+                with open('usernames.txt') as f:
+                    if u in f.read():
+                        break
+                    else:
+                        print("Error, Username is not recognized")
+                        x = input("Enter username: ")
+                        with open('usernames.txt') as f:
+                            if x in f.read():
+                                flag = True
+                                break
+                            else:
+                                flag = False
+    file.close()
+    file2.close()
+
+    p = input("Password: ")
+    pas = validatePass(p)
+    if pas is True:
+        mainPage()
+    else:
+        while pas is False:
+            print("Error, Incorrect password")
+            p = input("Enter password: ")
+            if validatePass(p):
+                break
+    mainPage()
 
         
 home('')
