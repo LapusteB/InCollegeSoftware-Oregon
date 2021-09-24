e443afd3c17cb5606b14117d6f446da495bbe065
Brian Lapuste <seblapuste@gmail.com>
2021-09-23 20:39:08 -0400

    Stories + video

--- Users.py
+++ Users.py
@@ -1,3 +1,4 @@
+import random
 from views import mainPage
 from validatePass import validatePass
 
@@ -12,12 +13,15 @@ class User:
 
 
 def home(user):
-    print("Please type either: 'Login' or 'Register")
+    play_story()
+    print("\nPlease type either: 'Login' or 'Register' or press '1' to see a video of a sucessful student who used InCollege!")
     a = input("What would you like to do: ")
     if (a == "register" or a == "Register"):
         register()
     elif (a == "Login" or a == "login"):
         login(user)
+    elif(int(a) == 1):
+        play_video()
     else:
         print("Choose a valid option")
         home('')
@@ -149,6 +153,39 @@ def username_exists(u):
         if u in f.read():
             return True
         return False
+
+def play_video():
+    print("Video is now playing\n")
+    a = input("Press '0' for home." )
+    if(int(a) == 0):
+        home('')
+
+def play_story():
+    n = random.randint(1, 3)
+    if n == 1:
+        story_1()
+    elif n == 2:
+        story_2()
+    elif n == 3:
+        story_3()
+    
+def story_1():
+    return print("John L. Miller, 25 years at Microsoft, Amazon, Google, etc. C++, C, Java, Basic, etc. PhD.\n" +
+    "'I loved computers. I taught myself to program in high school,\n" +
+    "and thought I was pretty good at it (Apple Basic and 6502 assembler)\n" +
+    "by the time I graduated. I got a job typesetting at a newspaper,\n" +
+    "and enrolled in university part time, taking programming classes.'\n")
+def story_2():
+    return print("Hazim Hardeman, Communications Program Graduate, 2015\nWhen " +
+    "Hazim Hardeman started at Community College of Philadelphia in 2012, his goals\n" +
+    "were to raise his GPA and transfer to Temple University. He accomplished that and much more,\n"
+    +"graduating from the College in 2015 with an associate degree in Communications with High Honors,")
+def story_3():
+    return print("Mark Zuckerberg has already accomplished much in his short life. In 2004,\n" +
+    "he launched Facebook with a handful of his fellow college students, and 10 years later, the\n" +
+    "website has more than 1 billion active users around the globe, and more than $12 billion in annual revenues.\n" +
+    "'Helping a billion people connect is amazing, humbling and by far the thing I am most proud of in my life.'")
+    
 
 
 if __name__ == "__home('')__":
