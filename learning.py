690bb5d43d5ad5772b30a971969b4d7b355525b2
Mihong <mihong@usf.edu>
2021-11-09 13:53:46 -0500

    learning done

--- learning.py
+++ learning.py
@@ -1,32 +1,34 @@
 def learningMenu(name):
-
-    print("Welcome to In College Learning\n")
-    print("")
-    print("-----------------------------------------------------------------")
-    print("| '1' to learn How to use In College learning                   |")
-    print("| '2' to learn Train the trainer                                |")
-    print("| '3' to learn Gamification of learning                         |")
-    print("| '4' to learn Understanding the Architectural desing Process   |")
-    print("| '5' to learn Project Management Simplified                    |")
-    print("| '0' to return to main page                                    |")
-    print("-----------------------------------------------------------------")
-    cmd = input("What would you like to do: ")
-
-    if (cmd == '1'):
-        InCollegeLearning(name)
-    elif (cmd == '2'):
-        Train(name)
-    elif (cmd == '3'):
-        Gamification(name)
-    elif (cmd == '4'):
-        Understanding(name)
-    elif (cmd == '5'):
-        Project(name)
-    elif (cmd == '0'):
-        return
-    else:
-        print("Invalid input, please try again")
+    cmd = ''
+    while(cmd!='0'):
+        print("Welcome to In College Learning\n")
         print("")
+        print("-----------------------------------------------------------------")
+        print("| '1' to learn How to use In College learning                   |")
+        print("| '2' to learn Train the trainer                                |")
+        print("| '3' to learn Gamification of learning                         |")
+        print("| '4' to learn Understanding the Architectural desing Process   |")
+        print("| '5' to learn Project Management Simplified                    |")
+        print("| '0' to return to main page                                    |")
+        print("-----------------------------------------------------------------")
+        cmd = input("What would you like to do: ")
+
+        if (cmd == '1'):
+            InCollegeLearning(name)
+        elif (cmd == '2'):
+            Train(name)
+        elif (cmd == '3'):
+            Gamification(name)
+        elif (cmd == '4'):
+            Understanding(name)
+        elif (cmd == '5'):
+            Project(name)
+        elif (cmd == '0'):
+            continue
+        else:
+            print("Invalid input, please try again")
+            print("")
+    return
 
 def InCollegeLearning(name):
     if(taken_Incollege(name)):
@@ -130,7 +132,7 @@ def taken_Understanding(name):
     return False
 
 def Project(name):
-    if(taken_Incollege(name)):
+    if(taken_Project(name)):
         decision = input("You have already taken this course, do you want to take it again?\n")
         if(decision == 'y'):
             print("You have now completed this training\n")
@@ -144,7 +146,7 @@ def Project(name):
         learningfile.close() 
         print("You have now completed this training")
 
-def taken_Understanding(name):
+def taken_Project(name):
     
     for line in open("learning.txt", "r"):
         if line != '\n':
